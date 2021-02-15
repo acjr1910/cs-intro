6ad3600c6d8705f20481bb81e592e3c5d7c7ba12
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2021-02-15 19:40:30 -0300

    solved: problem 2

--- unit_2/problem_set/problem_2.py
+++ unit_2/problem_set/problem_2.py
@@ -1,10 +1,23 @@
-balance = 3329
+balance = 3926
 annualInterestRate = 0.2
 
 monthly_interest_rate = annualInterestRate / 12.0
-monthly_unpaid_balance = balance - minimum
-balance = monthly_unpaid_balance + (monthly_interest_rate * balance)
 
 
 def calculate_minimum_fixed(balance):
-    return
+    unpaid_balance = balance
+    fixed_monthly_payment = 0
+    while unpaid_balance > 0:
+        for month in range(12):
+            unpaid_balance = unpaid_balance - fixed_monthly_payment
+            unpaid_balance = unpaid_balance + \
+                (monthly_interest_rate * unpaid_balance)
+        if unpaid_balance > 0:
+            fixed_monthly_payment += 10
+            unpaid_balance = balance
+        else:
+            break
+    return print('Lowest Payment:',  fixed_monthly_payment)
+
+
+calculate_minimum_fixed(balance)
